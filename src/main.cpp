b648d13fb44b8819178aef6c281c4e67787cd829
Michael Orr <michaeljamesorr@gmail.com>
2020-06-06 15:57:27 +1200

    added widgets to display texture with different amounts of processing on num keys

--- src/main.cpp
+++ src/main.cpp
@@ -14,26 +14,16 @@ int main() {
     int scale_x = 480;
     int scale_y = 360;
 
-    TexData texture1 = TexData::loadImage("textures/rangitoto.jpg");
-    TexData scaled_tex = nearest_neighbour_scale(texture1, scale_x, scale_y);
+    TexData original_tex = TexData::loadImage("textures/rangitoto.jpg");
+    TexData scaled_tex = nearest_neighbour_scale(original_tex, scale_x, scale_y);
     TexData gray_tex = convert_grayscale(scaled_tex);
     TexData edge_tex = sobel_edge_detect(gray_tex);
     TexData onebit_tex = onebit_posterize(edge_tex, 0.95f);
-    // gray_tex *= 0.3;
-    // onebit_tex += gray_tex;
-    // onebit_tex.clampValues();
     onebit_tex = invert(onebit_tex);
     
     float* canvas = new float[scale_x*scale_y*3]{0};
     std::shared_ptr<float> canvas_ptr(canvas);
     TexData canvas_tex(canvas_ptr, scale_x, scale_y, 3);
-
-    // int num_points = 50;
-    // glm::ivec2* points = GetRandomIntPoints(num_points*2, 0, scale_x-1, 0, scale_y-1);
-    // glm::vec3* colours = GetRandomHues(num_points, 0.7f, 0.8f);
-    // for(int i = 0; i < num_points; i++){
-    //     setRGBPixel(canvas_tex, points[i].x, points[i].y, ConvertHSVtoRGB(colours[i]));
-    // }
 
     TextureDataSource tex_data_source(canvas_tex, luminate::FLOW_3, &(*onebit_tex.getData()), 0.8f);
     std::shared_ptr<float> data_ptr(GetRandomFloats(200));
@@ -46,25 +36,53 @@ int main() {
 
     Window window = Window(1024, 882, "Percolate");
     window.initialise();
-    
-    window.addWidget(new LineWidget(0, 682, 1024, 200,
-                     glm::vec3(1.0f), 1.0f, &hist_data_source, hist_colours));
+
     TextureWidget* canvas_widget = new TextureWidget(0, 0, 1024, 682, 
                                                      glm::vec3(0.0f), 1.0f, &tex_data_source);
     window.addWidget(canvas_widget);
-    window.addWidget(new TextureWidget(0, 0, 1024, 682,
-                     glm::vec3(0.0f), 0.5f, onebit_tex));
+    
+    window.addWidget(new LineWidget(0, 682, 1024, 200,
+                     glm::vec3(1.0f), 1.0f, &hist_data_source, hist_colours));
+
+    TextureWidget* mask_widget = new TextureWidget(0, 0, 1024, 682,
+                                                   glm::vec3(0.0f), 0.5f, onebit_tex);
+    window.addWidget(mask_widget);
+
+    TextureWidget* edge_widget = new TextureWidget(0, 0, 1024, 682,
+                                                   glm::vec3(0.0f), 1.0f, edge_tex);
+    window.addWidget(edge_widget);
+    edge_widget->setVisible(false);
+
+    TextureWidget* original_widget = new TextureWidget(0, 0, 1024, 682,
+                                                   glm::vec3(0.0f), 1.0f, original_tex);
+    window.addWidget(original_widget);
+    original_widget->setVisible(false);
+
+    typedef struct {
+        TextureDataSource* tex_data_source;
+        TextureWidget* mask_widget;
+        TextureWidget* edge_widget;
+        TextureWidget* original_widget;
+    } WidgetContainer;
+
+    WidgetContainer widgets;
+    widgets.tex_data_source = &tex_data_source;
+    widgets.mask_widget = mask_widget;
+    widgets.edge_widget = edge_widget;
+    widgets.original_widget = original_widget;
 
     class CanvasKeyHandler : public KeyHandler{
         public:
             CanvasKeyHandler(void* params) : KeyHandler(params){};
             void key_handler(GLFWwindow* window, int key, int scancode, int action, int mods) override{
                 if(key == GLFW_KEY_SPACE && action == GLFW_PRESS){
-                    TextureDataSource* tex_data_source = (TextureDataSource*)(this->params);
+                    WidgetContainer* widgets = (WidgetContainer*)(this->params);
+                    TextureDataSource* tex_data_source = widgets->tex_data_source;
                     TexData canvas = tex_data_source->getData();
                     canvas.clear();
                 }else if(key == GLFW_KEY_ENTER && action == GLFW_PRESS){
-                    TextureDataSource* tex_data_source = (TextureDataSource*)(this->params);
+                    WidgetContainer* widgets = (WidgetContainer*)(this->params);
+                    TextureDataSource* tex_data_source = widgets->tex_data_source;
                     TexData canvas = tex_data_source->getData();
                     int num_points = 50;
                     glm::ivec2* points = GetRandomIntPoints(num_points*2, 0, canvas.getWidth()-1, 0, canvas.getHeight()-1);
@@ -72,11 +90,24 @@ int main() {
                     for(int i = 0; i < num_points; i++){
                         setRGBPixel(canvas, points[i].x, points[i].y, ConvertHSVtoRGB(colours[i]));
                     }
+                }else if(key == GLFW_KEY_1 && action == GLFW_PRESS){
+                    WidgetContainer* widgets = (WidgetContainer*)(this->params);
+                    widgets->original_widget->setVisible(true);
+                    widgets->edge_widget->setVisible(false);
+                }else if(key == GLFW_KEY_2 && action == GLFW_PRESS){
+                    WidgetContainer* widgets = (WidgetContainer*)(this->params);
+                    widgets->original_widget->setVisible(false);
+                    widgets->edge_widget->setVisible(true);
+                }else if(key == GLFW_KEY_3 && action == GLFW_PRESS){
+                    WidgetContainer* widgets = (WidgetContainer*)(this->params);
+                    widgets->original_widget->setVisible(false);
+                    widgets->edge_widget->setVisible(false);
                 }
+
             };
     };
 
-    window.addKeyHandler(new CanvasKeyHandler((void*)&tex_data_source));
+    window.addKeyHandler(new CanvasKeyHandler((void*)&widgets));
 
     class CanvasClickHandler : public MouseHandler{
         public:
