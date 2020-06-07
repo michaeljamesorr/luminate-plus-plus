c4a3a0ac1ecb9924d0ad02b6d2c196285eb9bc37
Michael Orr <michaeljamesorr@gmail.com>
2020-06-07 20:56:48 +1200

    some additional comments

--- src/datasource.cpp
+++ src/datasource.cpp
@@ -18,27 +18,54 @@ TextureDataSource::TextureDataSource(TexData texture, FilterKernel filter)
                                     :TextureDataSource(texture, filter, NULL, 1.0f){};
 
 bool TextureDataSource::fetch(){
+    // Apply filter to the current TexData and get new processed copy
     TexData filtered = apply_filter(this->data, this->filter, this->strength_mask, this->cutoff);
+    // Make new processed TexData available
     this->data = filtered;
+    // We got new data
     return true;
 };
 
+/* HistogramDataSource Methods */
+
+HistogramDataSource::HistogramDataSource(TextureDataSource* tex_data_source, int bins)
+                    :DataSource(getPixelHistogram(tex_data_source->getData(), bins)),
+                     tex_data_source(tex_data_source), bins(bins){};
+
+bool HistogramDataSource::fetch(){
+    // Get data from the TextureDataSource and convert it to a histogram
+    DataSeries new_series = getPixelHistogram(this->tex_data_source->getData(), this->bins);
+    // Make new histogram available
+    this->data = new_series;
+    // We got new data
+    return true;
+};
+
+
+/* Other functions */
+
 DataSeries luminate::getPixelHistogram(TexData texture, int bins){
+    // Access texture parameters
     float* src_data = texture.getData().get();
     int width = texture.getWidth();
     int height = texture.getHeight();
     int depth = texture.getDepth();
 
+    // Interval 0.0 to 1.0 divided into n bins of this size
     float bin_size = 1.0f/bins;
 
-    //TODO Check accuracy
+    // Create new array to hold histogram data - 4 channels
     float* hist_data = new float[bins*4]{0};
+    // for each pixel...
     for(int i = 0; i < width*height; i++){
         int pixel_idx = i*3;
+        // get bin indices for the r, g, b, and overall luminance values
+        // bin index is floor(x/bin_size) - int cast rounds down
         int r = (int)(src_data[pixel_idx]/bin_size);
         int g = (int)(src_data[pixel_idx+1]/bin_size);
         int b = (int)(src_data[pixel_idx+2]/bin_size);
         int l = (int)(pixel_luminance(&src_data[pixel_idx])/bin_size);
+        // add to tally for appropriate bins
         hist_data[r] += 1.0f;
         hist_data[bins + g] += 1.0f;
         hist_data[2*bins + b] += 1.0f;
@@ -48,15 +75,3 @@ DataSeries luminate::getPixelHistogram(TexData texture, int bins){
     DataSeries out_series(std::shared_ptr<float>(hist_data), bins, 4);
     return out_series;
 }
-
-/* HistogramDataSource Methods */
-
-HistogramDataSource::HistogramDataSource(TextureDataSource* tex_data_source, int bins)
-                    :DataSource(getPixelHistogram(tex_data_source->getData(), bins)),
-                     tex_data_source(tex_data_source), bins(bins){};
-
-bool HistogramDataSource::fetch(){
-    DataSeries new_series = getPixelHistogram(this->tex_data_source->getData(), this->bins);
-    this->data = new_series;
-    return true;
-};
--- src/draw.cpp
+++ src/draw.cpp
@@ -5,22 +5,27 @@
 using namespace luminate;
 
 void luminate::setRGBPixel(TexData texture, int x, int y, glm::vec3 colour){
+    // make sure the texture has three channels to work on
     assert(texture.getDepth()==3);
 
     int width = texture.getWidth();
     int height = texture.getHeight();
     int depth = 3;
 
+    // find pixel position in data array
     float* data = texture.getData().get();
 
     int pixel_idx = y*width*depth + x*depth;
 
+    // set pixel elements equal to colour elements
     data[pixel_idx] = colour.x;
     data[pixel_idx+1] = colour.y;
     data[pixel_idx+2] = colour.z;
 }
 
+/* Used to draw a line which is longer in the x axis i.e. <= 45 degree gradient */
 void plot_line_low(TexData texture, int x0, int y0, int x1, int y1, glm::vec3 colour){
+    // Bresenham line algorithm https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
     int width = texture.getWidth();
     int height = texture.getHeight();
     int depth = 3;
@@ -50,7 +55,9 @@ void plot_line_low(TexData texture, int x0, int y0, int x1, int y1, glm::vec3 co
     }
 }
 
+/* Used to draw a line which is longer in the y axis i.e. >= 45 degree gradient */
 void plot_line_high(TexData texture, int x0, int y0, int x1, int y1, glm::vec3 colour){
+    // Bresenham line algorithm https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
     int width = texture.getWidth();
     int height = texture.getHeight();
     int depth = 3;
@@ -87,10 +94,13 @@ void luminate::drawRGBLine(TexData texture, int x0, int y0, int x1, int y1, glm:
     int height = texture.getHeight();
     int depth = 3;
 
+    // check input values make sense
     assert(x0 >= 0 && y0 >= 0 && x1 >= 0 && y1 >= 0);
     assert(x0 < width and y0 < height and x1 < width and y1 < height);
 
+    // determine if gradient of line is low or high (abs value)
     if(abs(y1-y0) < abs(x1 - x0)){
+        // always start at lower x-coordinate
         if (x0 > x1){
             plot_line_low(texture, x1, y1, x0, y0, colour);
         }else{
@@ -98,6 +108,7 @@ void luminate::drawRGBLine(TexData texture, int x0, int y0, int x1, int y1, glm:
         }
     }else{
         if(y0 > y1){
+            // always start at lower y-coordinate
             plot_line_high(texture, x1, y1, x0, y0, colour);
         }else{
             plot_line_high(texture, x0, y0, x1, y1, colour);
